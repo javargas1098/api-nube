be5a1b875ea98603c8ebed8e15f945deec1875ee
Javier Vargas <javier_vargas@epam.com>
2021-11-26 21:33:47 -0500

    s3

--- flaskr/vistas/vistas.py
+++ flaskr/vistas/vistas.py
@@ -67,7 +67,7 @@ class VistaTasks(Resource):
         filename = secure_filename(file.filename)
         filename = '{}.{}'.format(os.path.splitext(filename)[0] + str(uuid.uuid4()),
                                     os.path.splitext(filename)[1])  # Build input name    
-        MYDIR = os.path.dirname(__file__)                                                    
+        MYDIR = os.path.dirname(__file__).replace("vistas", "")                                                 
         output = os.path.join(MYDIR + "/" + current_app.config['UPLOAD_FOLDER'], filename)
         file.save(output)        
         uuidSelected = uuid.uuid4()
