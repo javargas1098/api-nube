0aa93c1b1346a3a9a67040973f2e259e15818cc7
Javier Vargas <javier_vargas@epam.com>
2021-11-26 23:41:59 -0500

    s3

--- flaskr/vistas/vistas.py
+++ flaskr/vistas/vistas.py
@@ -70,7 +70,7 @@ class VistaTasks(Resource):
                                     os.path.splitext(filename)[1])  # Build input name    
         MYDIR = os.path.dirname(__file__).replace("vistas", "").replace("/app/","")                                                  
         output = os.path.join(current_app.config['UPLOAD_FOLDER_FACES'], filename)
-        file.save(output) 
+        # file.save(output) 
         print(output)
         print("guarde nene")
         sendFile = {"file": file}
