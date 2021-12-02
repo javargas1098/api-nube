e1ec494634564ef015a7316250dc83048b27d9bd
javargas1098 <ja.vargas1098@gmail.com>
2021-12-01 22:22:40 -0500

    changes

--- flaskr/vistas/vistas.py
+++ flaskr/vistas/vistas.py
@@ -73,8 +73,9 @@ class VistaTasks(Resource):
         output = os.path.join(current_app.config['UPLOAD_FOLDER_FACES'], filename)
         # os.mkdir(current_app.config['UPLOAD_FOLDER_FACES'], 'tmp')
         # os.path.join()
+        print(os.path.dirname(__file__))
         print(output)
-        file.save(output) 
+        print(file.save(output) )
         
        
         uuidSelected = uuid.uuid4()
