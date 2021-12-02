ba095f3846feb7a452e9478bbf410f1370d0c544
javargas1098 <ja.vargas1098@gmail.com>
2021-12-01 21:27:31 -0500

    changes

--- flaskr/vistas/vistas.py
+++ flaskr/vistas/vistas.py
@@ -71,8 +71,7 @@ class VistaTasks(Resource):
                                     os.path.splitext(filename)[1])  # Build input name    
         MYDIR = os.path.dirname(__file__).replace("vistas", "").replace("/app/","")                                                  
         output = os.path.join(current_app.config['UPLOAD_FOLDER_FACES'], filename)
-        m=os.mkdir(current_app.config['UPLOAD_FOLDER_FACES'], 'tmp')
-        print(m)
+        os.mkdir(current_app.config['UPLOAD_FOLDER_FACES'], 'tmp')
         # os.path.join()
         # file.save(output) 
         print(output)
